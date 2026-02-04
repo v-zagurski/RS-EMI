b5be325ac2b8e249ac6023a825a03581d2bc6b9a
V. Zagurski <arbeitersolidaritat@gmail.com>
2026-02-04 09:12:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from RsSpectrumAnalyzer import RsSpectrumAnalyzer
 
 san = RsSpectrumAnalyzer('TCPIP0::169.254.19.12::inst0::INSTR')
 san.setup_display(unit='DBUV', ref=80, scale=100)
-san.setup_meas(trac_mode='MAXH', cont=0, av_num=1, det='RMS', fstart=20, fstop=50, points=500, rbw=100, t=0.01, att=20)
+san.setup_meas(trac_mode='MAXH', cont=0, av_num=1, det='RMS',
+               fstart=20, fstop=50, points=500,
+               rbw=100, t=0.01, att=20)
 print(san._idn)
 print(san._status)
 print(san.core.query('FORM?'))
